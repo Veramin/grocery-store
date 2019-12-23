ebf2b623ddd7f1b10910d89f0e5beadc74490ad5
vera_min <vera_min@163.com>
2019-12-23 14:05:54 +0800

    js function definition

--- js-tutorial/01/index.js
+++ js-tutorial/01/index.js
@@ -1,15 +1,38 @@
-console.log('hello, vera');
+console.log('hello, vera')
 
 let person = {
   name: 'vera',
   age: 24
-};
+}
 
 // Dot Notation
-person.name = 'Frank';
+person.name = 'Frank'
 
 // Bracket Notation
-let selection = 'name';
-person[selection] = 'Mary';
+let selection = 'name'
+person[selection] = 'Mary'
 
-console.log(person.name);
+console.log(person.name)
+
+// Array
+let selectedColors = ['red', 'bule']
+console.log(selectedColors[1])
+
+selectedColors[2] = 6
+console.log(selectedColors)
+console.log(selectedColors.length)
+
+// Function
+// Performing a task
+function greet (name, lastName) {
+  console.log('Hello, ' + name + ' ' + lastName);
+}
+greet('世界, 你的')
+
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+let number = square(2);
+console.log(number)
